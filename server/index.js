8d963cd1c4addd2b6ba05e2f5f4412ee68c3ab43
Angelchild00 <jaden.mardini@proton.me>
2025-10-30 19:40:19 -0400

    Task-40: Implement GPS listener to mock

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const GPSListener = require('./gpsListener');
 const app = express();
 const PORT = 3000;
 
+// Initialize GPS listener
+const gpsListener = new GPSListener();
+
 app.get('/', (req, res) => {
   res.send('Group 1F25 server is running 🚗💨');
+});
+
+app.get('/gps/current', (req, res) => {
+  res.json(gpsListener.getCurrentPosition());
+});
+
+app.get('/gps/start', (req, res) => {
+  gpsListener.startListening((data) => {
+    console.log('GPS Update:', data);
+  });
+  res.json({ message: 'GPS listening started' });
+});
+
+app.get('/gps/stop', (req, res) => {
+  gpsListener.stopListening();
+  res.json({ message: 'GPS listening stopped' });
 });
 
 app.listen(PORT, () => {
